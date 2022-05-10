1a9e400328603051a4521fb62a4b28edfa09ccc2
SandroRZovadelli <zovadelii32@gmail.com>
2022-05-10 08:48:00 -0300

    new commit

---
+++ src/app/mock-produtos.ts
@@ -0,0 +1,14 @@
+import { Produto } from "./produto";
+
+export const PRODUTOS: Produto[] = [
+  { id: 1, name: 'Caneta', preco: 1.50, qtdEstoque: 1000, categoria: 'BIC-azul'},
+  { id: 2, name: 'Borracha', preco: 1.40, qtdEstoque: 500, categoria: 'Faber Castell'},
+  { id: 3, name: 'Caderno', preco: 14.99, qtdEstoque: 32, categoria: 'Tilibra 160-fls' },
+  { id: 4, name: 'Lápis de cor', preco: 39.99, qtdEstoque: 810, categoria: 'BIC' },
+  { id: 5, name: 'Lápis', preco: 1.20, qtdEstoque: 260, categoria: 'Faber Castell HB-2' },
+  { id: 6, name: 'Apontador', preco: 3.00, qtdEstoque: 250, categoria: 'Faber Castell' },
+  { id: 7, name: 'Mochila', preco: 139.99, qtdEstoque: 30, categoria: 'Diversos' },
+  { id: 8, name: 'Giz de cera', preco: 9.99, qtdEstoque: 122, categoria: 'Leoleo' },
+  { id: 9, name: 'Papel sulfite', preco: 21.99, qtdEstoque: 84, categoria: 'Chamex - 500-un' },
+  { id: 10, name: 'Estojo', preco: 29.99, qtdEstoque: 821, categoria: 'Diversos' }
+];
